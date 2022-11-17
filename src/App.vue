e4c0b9fe20157d327cd02d48809f4b41c517c737
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-11-17 17:47:35 +0300

    add antd

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
+<template>
+  <a-button type="primary">Primary Button</a-button>
+  {{value}}
+  <a-input v-model:value="value" placeholder="Basic usage" />
+</template>
+
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+import { ref } from 'vue';
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+const value = ref<string>('')
+</script>
 
 <style scoped>
 .logo {
